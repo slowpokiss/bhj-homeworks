3be45a0c036919b3f4a2851367052883ba1c8d3c
Michel <hltvexe@mail.ru>
2023-06-06 22:36:27 +0300

    mole2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,19 +1,28 @@
-for (let i = 0; i < 10; i++) {
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+function getHole(index) {
+  return document.getElementById(`hole${index}`)
+}
+
+for (let i = 0; i < 9; i++) {
   getHole(i + 1).onclick = function () {
     if (getHole(i + 1).className.includes( 'hole_has-mole' )) {     
-      dead.innerHTML++;
-      if (Number(dead.innerHTML) === 10) {
-        dead.innerHTML = 0;
-        lost.innerHTML = 0;
-        alert("Победа!")
-      }
+      dead.textContent++;
     } else {
-      lost.innerHTML++;
-      if (Number(dead.innerHTML) === 5) {
-        dead.innerHTML = 0;
-        lost.innerHTML = 0;
-        alert("Поражение")
-      }
-    }  
+      lost.textContent++;
+    }
+
+    if (Number(dead.textContent) === 10) {
+      dead.textContent = 0;
+      lost.textContent = 0;
+      alert("Победа!")
+    }
+
+    if (Number(lost.textContent) === 5) {
+      dead.textContent = 0;
+      lost.textContent = 0;
+      alert("Поражение")
+    }
   }
 }
