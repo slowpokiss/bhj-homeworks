db3e49e6469142437957b0f6b3356cd33f51a61b
Michel <hltvexe@mail.ru>
2023-06-13 22:45:27 +0300

    book-reader2

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,25 +1,18 @@
 const fs = document.querySelectorAll('.font-size');
 const bookCont = document.querySelector('.book');
 
-
-fs.forEach((el, ind) => {
+fs.forEach((el) => {
   el.addEventListener('click', function(ev)  {
     ev.preventDefault();
     const currFs = document.querySelector(".font-size_active");
     currFs.classList.remove('font-size_active');
     el.classList.add('font-size_active');
     
-    if (ind === 0) {
-      bookCont.classList.remove('book_fs-big');
-      bookCont.classList.add('book_fs-small');
-    } if (ind === 1) {
-      bookCont.classList.remove('book_fs-small');
-      bookCont.classList.remove('book_fs-big');
-    } if (ind === 2) {
-      bookCont.classList.remove('book_fs-small');
-      bookCont.classList.add('book_fs-big');
+    const size = ev.target.dataset.size;
+    if (size) {
+      bookCont.className = `book__content book_fs-${size}`;
+    } else {
+      bookCont.className = "book__content";
     }
-  
   })
 });
-
