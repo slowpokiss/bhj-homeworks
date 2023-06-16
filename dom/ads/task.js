5691e9ce4c0dcd4bf8237887d597e9e42c1b0236
Michel <hltvexe@mail.ru>
2023-06-16 22:40:39 +0300

    ads3

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,15 +1,16 @@
 setInterval(() => {
   let rcase = document.querySelector('.rotator__case_active');
-
+  
   if (rcase.nextElementSibling === null) { 
-    rcase.setAttribute("style", "color: " + String(rcase.getAttribute('data-color')))
     rcase.classList.remove('rotator__case_active');
     rcase.parentElement.firstElementChild.classList.add('rotator__case_active');
-  } else {  
-    //rcase.setAttribute("style", "color: " + String(rcase.getAttribute('data-color')))
+  } else { 
+    if (rcase.parentElement.firstElementChild.getAttribute('style') === null) {
+      rcase.parentElement.firstElementChild.setAttribute("style", "color: " + String(rcase.parentElement.firstElementChild.getAttribute('data-color')))
+    }
     rcase.nextElementSibling.setAttribute("style", "color: " + String(rcase.nextElementSibling.getAttribute('data-color')))
+    
     rcase.classList.remove('rotator__case_active');
     rcase.nextElementSibling.classList.add('rotator__case_active');
-
   }
 }, 1000);
