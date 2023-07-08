23279ab823e2cd988c4251c409c5e232afe73eb9
Michel <hltvexe@mail.ru>
2023-07-08 15:58:18 +0300

    tooltip3

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,17 +1,15 @@
-const ttips = document.querySelectorAll('.has-tooltip')
+const ttips = document.querySelectorAll('.has-tooltip');
 
 ttips.forEach(el => {
   const tip = document.createElement('div');
   tip.textContent = el.title; 
   tip.classList.add('tooltip');
   el.after(tip);
-
-  //tip.classList.add('tooltip_active')
+  
   el.addEventListener('click', (ev) => {
     ev.preventDefault();
-    tip.classList.toggle('tooltip_active')
-
-    console.log(el.nextElementSibling.getBoundingClientRect().height);
+    tip.classList.toggle('tooltip_active');
     tip.style.position = 'absolute';
+    tip.style.left = String(el.getBoundingClientRect().x) + 'px';
   })
 });
